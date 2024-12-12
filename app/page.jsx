61237fbdf3b0a4c746cd5f02202f587f51b5c448
you tae hee <f_hyang@naver.com>
2024-12-12 09:40:55 +0900

    feat: 계산기 app page.jsx에서 분리

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import VersionOne from "../component/versionOne/VersionOne";
+import styles from "./home.scss";
+export default function Home() {
+  return (
+    <div className="home_containerWrap">
+      <VersionOne />
+    </div>
+  );
+}
